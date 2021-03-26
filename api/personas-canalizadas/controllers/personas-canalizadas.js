bc34c92d43fe6127136e08912bb3f2b1cf514e1e
ahc2806 <alonsohernandezcortez@gmail.com>
2021-03-26 07:22:54 -0600

    Cambios finales, es hoy

--- api/personas-canalizadas/controllers/personas-canalizadas.js
+++ api/personas-canalizadas/controllers/personas-canalizadas.js
@@ -1,8 +1,21 @@
-"use strict";
+'use strict';
+
+const { sanitizeEntity } = require('strapi-utils');
 
 module.exports = {
     
-    findPersonas(params, populate) {
-        return strapi.query('personas').findOne(params, populate);
+    async findAll() {
+        try {
+            const persons_canalize = await strapi.services['personas-canalizadas'].find();
+            
+            persons_canalize.map(async (data, i) => {
+                persons_canalize[i].alumno.persona = await strapi.services['personas-canalizadas'].findPersonas({ id: data.alumno.persona });
+            });
+
+            return sanitizeEntity(persons_canalize, { model: strapi.models['personas-canalizadas'] });
+        } catch (error) {
+            console.log(error);
+            return ctx.throw(400, error);
+        }
     },
 };
